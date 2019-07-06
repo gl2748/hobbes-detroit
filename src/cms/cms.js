530813269e4b4083f6bbb40fa5519dd6a583e3d0
/ /\ / /\/ <imaitland@gmail.com>
2019-07-06 12:57:37 -0400

    remove cloudinary again

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,7 +7,6 @@ import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
-CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
