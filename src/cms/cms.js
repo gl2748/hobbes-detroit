a155c0b1a8cb2d7998d3ff87e90bfb0709dd8601
/ /\ / /\/ <imaitland@gmail.com>
2019-07-08 07:03:26 -0400

    add project post preview template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,10 +5,12 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import ProjectPostPreview from './preview-templates/ProjectPostPreview'
 
 CMS.registerMediaLibrary(uploadcare);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('project', ProjectPostPreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
