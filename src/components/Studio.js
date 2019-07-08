d20f3a740783e19c931536be2e8e448f4999f63e
/ /\ / /\/ <imaitland@gmail.com>
2019-07-07 22:41:44 -0400

    wip sunday eve

---
+++ src/components/Studio.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Studio = ({title, description, address, phone, email, social} ) => {
+  return (
+    <div>
+    <h1>
+      {title}
+    </h1>
+      <p>{description}</p>
+      <p>{address}</p>
+      <p>{phone}</p>
+      <p>{email}</p>
+      <p>{social[0].title}</p>
+      <p>{social[0].url}</p>
+  </div>
+  )
+}
+
+Studio.propTypes = {
+  title: PropTypes.string,
+  address: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+  social: PropTypes.arrayOf(PropTypes.shape({
+    url: PropTypes.string,
+    title: PropTypes.string
+  }))
+}
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,87 +1,18 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-
 import Layout from '../components/Layout'
 import ProjectRoll from '../components/ProjectRoll'
+import StudioContainer from '../containers/StudioContainer'
 
-export const IndexPageTemplate = ({
-  title,
-  description,
-  address,
-  phone,
-  email,
-  social
-}) => {
-return (
-  <div>
-    <div>Featured post gallery goes here</div>
-    <div>Project Roll:</div>
-    <ProjectRoll />
-    <h1>
-      {title}
-    </h1>
-      <p>{description}</p>
-      <p>{address}</p>
-      <p>{phone}</p>
-      <p>{email}</p>
-      <p>{social[0].title}</p>
-      <p>{social[0].url}</p>
-  </div>
-)}
-
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
-
-const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+const IndexPage = () => {
   return (
     <Layout>
-      <IndexPageTemplate
-        title={frontmatter.title}
-        description={frontmatter.description}
-        address={frontmatter.address}
-        email={frontmatter.email}
-        phone={frontmatter.phone}
-        social={frontmatter.social}
-      />
+      <div>
+        <div>Featured post gallery goes here</div>
+        <ProjectRoll />
+        <StudioContainer />
+      </div>
     </Layout>
   )
 }
 
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
-
 export default IndexPage
-
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        description
-        address
-        phone
-        email
-        social {
-          url
-          title
-        }
-      }
-    }
-  }
-`
