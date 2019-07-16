bb1216c0a01a766d4017d8923fc307e507c92ec0
/ /\ / /\/ <imaitland@gmail.com>
2019-07-16 19:02:08 -0400

    run git format

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,9 +4,9 @@ const { createFilePath } = require('gatsby-source-filesystem')
 const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions
+  const { createPage } = actions
 
-    return graphql(`
+  return graphql(`
     {
       allMarkdownRemark(limit: 1000) {
         edges {
@@ -24,70 +24,72 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   `).then(result => {
-        if (result.errors) {
-            result.errors.forEach(e => console.error(e.toString()))
-            return Promise.reject(result.errors)
-        }
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
 
-        const posts = result.data.allMarkdownRemark.edges
+    const posts = result.data.allMarkdownRemark.edges
 
-        posts.forEach(edge => {
-            const id = edge.node.id
-            let newPost = {}
-            if (String(edge.node.frontmatter.templateKey) === "null") {
-                console.log('It looks like you have a markdown file with improperly formatted or absent front matter, in this case the template key field is missing, consider either hand-coding a markdown file with the correct front matter or else deploying your latest changes to the netlify branch and using the CMS to create a new page / piece of content. Note: once you have done that in the CMS you will need to pull down the changes.')
-                return
-            }
-            newPost = {
-                path: edge.node.fields.slug,
-                tags: edge.node.frontmatter.tags,
-                component: path.resolve(
-                    `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-                ),
-                // additional data can be passed via context
-                context: {
-                    id,
-                },
-            }
-            createPage(newPost)
-        })
-
-        // Tag pages:
-        let tags = []
-        // Iterate through each post, putting all found tags into `tags`
-        posts.forEach(edge => {
-            if (_.get(edge, `node.frontmatter.tags`)) {
-                tags = tags.concat(edge.node.frontmatter.tags)
-            }
-        })
-        // Eliminate duplicate tags
-        tags = _.uniq(tags)
-
-        // Make tag pages
-        tags.forEach(tag => {
-            const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-            createPage({
-                path: tagPath,
-                component: path.resolve(`src/templates/tags.js`),
-                context: {
-                    tag,
-                },
-            })
-        })
+    posts.forEach(edge => {
+      const id = edge.node.id
+      let newPost = {}
+      if (String(edge.node.frontmatter.templateKey) === 'null') {
+        console.log(
+          'It looks like you have a markdown file with improperly formatted or absent front matter, in this case the template key field is missing, consider either hand-coding a markdown file with the correct front matter or else deploying your latest changes to the netlify branch and using the CMS to create a new page / piece of content. Note: once you have done that in the CMS you will need to pull down the changes.'
+        )
+        return
+      }
+      newPost = {
+        path: edge.node.fields.slug,
+        tags: edge.node.frontmatter.tags,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id,
+        },
+      }
+      createPage(newPost)
     })
+
+    // Tag pages:
+    let tags = []
+    // Iterate through each post, putting all found tags into `tags`
+    posts.forEach(edge => {
+      if (_.get(edge, `node.frontmatter.tags`)) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+    // Eliminate duplicate tags
+    tags = _.uniq(tags)
+
+    // Make tag pages
+    tags.forEach(tag => {
+      const tagPath = `/tags/${_.kebabCase(tag)}/`
+
+      createPage({
+        path: tagPath,
+        component: path.resolve(`src/templates/tags.js`),
+        context: {
+          tag,
+        },
+      })
+    })
+  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-    const { createNodeField } = actions
-    fmImagesToRelative(node) // convert image paths for gatsby images
+  const { createNodeField } = actions
+  fmImagesToRelative(node) // convert image paths for gatsby images
 
-    if (node.internal.type === `MarkdownRemark`) {
-        const value = createFilePath({ node, getNode })
-        createNodeField({
-            name: `slug`,
-            node,
-            value,
-        })
-    }
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
 }
