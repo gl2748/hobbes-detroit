c79e56bf25262dfe6116b63c6da91664275bfd87
Iain Maitland <iain@iainmaitland.com>
2019-07-17 16:06:06 -0400

    more ts work

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: `${__dirname}/src/cms/cms.ts`,
       },
     },
     {
--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -1,15 +1,15 @@
 import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 
-import { IndexPagePreview } from "./preview-templates/IndexPagePreview";
-import { ProjectPostPreview } from "./preview-templates/ProjectPostPreview";
-import { LegalPagePreview } from "./preview-templates/LegalPagePreview";
 import { CookiePagePreview } from "./preview-templates/CookiePagePreview";
+import { IndexPagePreview } from "./preview-templates/IndexPagePreview";
+import { LegalPagePreview } from "./preview-templates/LegalPagePreview";
+import { ProjectPostPreview } from "./preview-templates/ProjectPostPreview";
 
 
 CMS.registerMediaLibrary(uploadcare);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("project", ProjectPostPreview);
-CMS.registerPreviewTemplage("cookie", CookiePagePreview);
-CMS.registerPreviewTemplage("legal", LegalPagePreview);
+CMS.registerPreviewTemplate("cookie", CookiePagePreview);
+CMS.registerPreviewTemplate("legal", LegalPagePreview);
