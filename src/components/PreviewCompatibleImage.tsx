65a16c6a4e3723ce3c8cbaa141c847a7ea9a398b
/ /\ / /\/ <imaitland@gmail.com>
2019-07-21 18:21:07 -0400

    ts PreviewCompatibleImage

--- src/components/PreviewCompatibleImage.tsx
+++ src/components/PreviewCompatibleImage.tsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Img from 'gatsby-image'
+import React from "react";
+import PropTypes from "prop-types";
+import Img, { FluidObject } from "gatsby-image";
 
-export const PreviewCompatibleImage = ({ imageInfo }) => {
-  const imageStyle = { borderRadius: '5px' }
-  const { alt = '', childImageSharp, image } = imageInfo
+export interface IPreviewCompatibleImageProps {
+  imageInfo: {
+    alt: string;
+    childImageSharp: {
+      fluid: FluidObject;
+    };
+    style: {};
+    image: {
+      childImageSharp: {
+        fluid: FluidObject;
+      };
+    };
+  };
+}
+
+export const PreviewCompatibleImage: React.FC<IPreviewCompatibleImageProps> = ({
+  imageInfo
+}: IPreviewCompatibleImageProps) => {
+  const imageStyle = { borderRadius: "5px" };
+  const { alt = "", childImageSharp, image } = imageInfo;
 
   if (!!image && !!image.childImageSharp) {
     return (
       <Img style={imageStyle} fluid={image.childImageSharp.fluid} alt={alt} />
-    )
+    );
   }
 
   if (!!childImageSharp) {
-    return <Img style={imageStyle} fluid={childImageSharp.fluid} alt={alt} />
+    return <Img style={imageStyle} fluid={childImageSharp.fluid} alt={alt} />;
   }
 
-  if (!!image && typeof image === 'string')
-    return <img style={imageStyle} src={image} alt={alt} />
+  if (!!image && typeof image === "string")
+    return <img style={imageStyle} src={image} alt={alt} />;
 
-  return null
-}
-
-PreviewCompatibleImage.propTypes = {
-  imageInfo: PropTypes.shape({
-    alt: PropTypes.string,
-    childImageSharp: PropTypes.object,
-    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
-    style: PropTypes.object,
-  }).isRequired,
-}
-
+  return null;
+};
