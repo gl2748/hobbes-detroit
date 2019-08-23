dc2f6a78c25d0361025d98df5519874df3929f15
Mike Schutte <tmikeschutte@gmail.com>
2019-08-23 10:40:17 -0400

    Add main css and update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,4 @@
-import { configure } from "@storybook/react";
+import {configure} from "@storybook/react";
+import "../src/components/main.css";
 // automatically import all files ending in *.stories.tsx
 configure(require.context("../src", true, /\.stories\.tsx$/), module);
