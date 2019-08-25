94d217416a2e16400130efa9cb9053441e3d8982
Mike Schutte <tmikeschutte@gmail.com>
2019-08-25 14:26:13 -0400

    Add outside click container to drawer

--- src/components/HobDrawer/index.tsx
+++ src/components/HobDrawer/index.tsx
@@ -1,27 +1,37 @@
 import styled from "@emotion/styled";
-import React, { ReactNode } from "react";
+import React, { MouseEvent, ReactNode } from "react";
 import { HobIconButton } from "../HobIconButton";
 
 export interface IDrawerProps {
   isVisible: boolean;
   children: ReactNode;
   onClose: () => void;
+  className?: string;
 }
 
-const Drawer = styled.div`
-  padding: 1.25rem;
+const Container = styled.div`
+  width: 0;
   position: absolute;
-  height: 100vh;
-  z-index: 100;
-  left: -100%;
+  left: 0;
   top: 0;
+  overflow: hidden;
+  z-index: 100;
+  transition: width var(--hob-transition-duration);
+  height: 100vh;
+
+  &.hob-drawer-container--visible {
+    width: 100vw;
+  }
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+const Drawer = styled.div`
+  position: relative;
+  padding: 1.25rem;
+  height: 100%;
   transition: left var(--hob-transition-duration);
   background-color: var(--hob-color--light);
   width: 447px;
-  &.hob-drawer--visible {
-    left: 0;
-  }
-
   .hob-icon-button--close {
     position: absolute;
     top: 8px;
@@ -32,20 +42,35 @@ const Drawer = styled.div`
 export const HobDrawer: React.FC<IDrawerProps> = ({
   isVisible,
   children,
-  onClose
+  onClose,
+  className
 }) => {
+  const onlyParent = (fn: (e: MouseEvent<HTMLDivElement>) => void) => (
+    e: MouseEvent<HTMLDivElement>
+  ) => {
+    if (e.currentTarget === e.target) {
+      fn(e);
+    }
+  };
   return (
-    <Drawer
-      className={`hob-drawer hob-drawer--${isVisible ? "visible" : "hidden"}`}
+    <Container
+      className={`hob-drawer-container hob-drawer-container--${
+        isVisible ? "visible" : "hidden"
+      } ${className}`}
+      onClick={onlyParent(onClose)}
     >
-      <HobIconButton
-        onClick={onClose}
-        name="close"
-        size="sm"
-        variant="text"
-        color="primary"
-      />
-      {children}
-    </Drawer>
+      <Drawer
+        className={`hob-drawer hob-drawer--${isVisible ? "visible" : "hidden"}`}
+      >
+        <HobIconButton
+          onClick={onClose}
+          name="close"
+          size="sm"
+          variant="text"
+          color="primary"
+        />
+        {children}
+      </Drawer>
+    </Container>
   );
 };
