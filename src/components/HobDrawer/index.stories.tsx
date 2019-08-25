1f7635940ff59bfcdf8c0935f0a0085c33233663
Mike Schutte <tmikeschutte@gmail.com>
2019-08-25 12:15:02 -0400

    Add content to drawer story

--- src/components/HobDrawer/index.stories.tsx
+++ src/components/HobDrawer/index.stories.tsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import { HobDrawer } from ".";
 import { HobButton } from "../HobButton";
 import { HobLetters } from "../HobLetters";
+import { HobLogo } from "../HobLogo";
+import { HobTextField } from "../HobTextField";
+import { HobTypography } from "../HobTypography";
 
 export default {
   title: "Drawer"
@@ -20,7 +23,9 @@ const Container = styled.div`
 export const visible = () => (
   <Container>
     <HobDrawer isVisible={true} onClose={action("closed")}>
-      hello
+      <HobLetters size="lg" />
+      <HobTypography variant="body1">This is great</HobTypography>
+      <HobLogo />
     </HobDrawer>
   </Container>
 );
@@ -28,10 +33,14 @@ export const visible = () => (
 export const withState = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleDrawer = () => setIsOpen(!isOpen);
+  const onChange = action("change");
   return (
     <Container>
       <HobDrawer isVisible={isOpen} onClose={toggleDrawer}>
         <HobLetters size="lg" />
+        <HobTextField value="sweet" onChange={onChange} />
+        <HobTypography variant="body1">This is great</HobTypography>
+        <HobLogo />
       </HobDrawer>
 
       <HobButton variant="contained" color="secondary" onClick={toggleDrawer}>
