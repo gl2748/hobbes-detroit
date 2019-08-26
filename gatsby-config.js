5337ef99ea95d6b0c20aa1730e96886bb5e2dd00
Iain Maitland <iain@iainmaitland.com>
2019-08-26 12:05:31 -0400

    remove options from gatsby-plugin-purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,10 +70,6 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/main.css"] // applies purging only on the bulma css file
-      }
     }, // must be after other CSS plugins
     "gatsby-plugin-netlify" // make sure to keep it last in the array, for plugin settings see netlify.toml
   ],
