e0278c380b50901d11689065f1965948212f5fa4
Iain Maitland <iain@iainmaitland.com>
2019-08-26 11:50:06 -0400

    tidy up the uploadcare source plugin

--- plugins/gatsby-transformer-uploadcare/normalize.ts
+++ plugins/gatsby-transformer-uploadcare/normalize.ts
@@ -1,37 +1,14 @@
-import crypto from 'crypto';
-import stringify from 'json-stringify-safe';
 import deepMapKeys from 'deep-map-keys';
 import nanoid from 'nanoid';
 import chalk from 'chalk';
-import { NodeInput, Reporter, Node, Actions } from 'gatsby';
 
 const log = console.log;
-
-/**
- * Encrypts a String using md5 hash of hexadecimal digest.
- *
- */
-const digest = (str: string) =>
-  crypto
-    .createHash(`md5`)
-    .update(str)
-    .digest(`hex`);
 
 // Prefix to use if there is a conflict with key name
 const conflictFieldPrefix = `uploadcareTransformer_`;
 
 // Keys that will conflict with graphql
 const restrictedNodeFields = [`id`, `children`, `parent`, `fields`, `internal`];
-
-export interface ICreateNodesFromEntitiesParams {
-  entities: any[];
-  createNode: Actions['createNode'];
-  reporter: Reporter;
-  parentNodeId: string;
-  parentNode: Node;
-  createParentChildLink: Actions['createParentChildLink'];
-  createNodeField: Actions['createNodeField'];
-}
 
 /**
  * Validate the GraphQL naming convetions & protect specific fields.
@@ -72,7 +49,7 @@ export const getValidKey = ({
 };
 
 // Standardize ids + make sure keys are valid.
-export const standardizeKeys = (entities: any[]) =>
+export const standardizeKeys = (entities: any[]) => () =>
   entities.map(e =>
     deepMapKeys(e, key =>
       key === `ID` ? getValidKey({ key: `id` }) : getValidKey({ key })
@@ -80,14 +57,14 @@ export const standardizeKeys = (entities: any[]) =>
   );
 
 // Generate a unique id for each entity
-export const createGatsbyIds = (createNodeId: any, entities: any[]) =>
+export const createGatsbyIds = (createNodeId: Function) => (entities: any[]) =>
   entities.map(e => {
     e.id = createNodeId(`${nanoid()}`);
     return e;
   });
 
 // Add entity type to each entity
-export const createEntityType = (entityType: string, entities: any[]) => {
+export const createEntityType = (entityType: string) => (entities: any[]) => {
   return entities.map(e => {
     e.__type = entityType;
     return e;
--- plugins/gatsby-transformer-uploadcare/sourceNodes.ts
+++ plugins/gatsby-transformer-uploadcare/sourceNodes.ts
@@ -1,130 +1,120 @@
+import fp from 'lodash/fp';
+
 import {
-  GatsbyNode,
-  PluginOptions,
-  SourceNodesArgs,
-  NodePluginArgs,
+    GatsbyNode,
+    PluginOptions,
+    NodePluginArgs,
+    Node
 } from 'gatsby';
 import { fetch } from './fetch';
 import {
-  standardizeKeys,
-  createEntityType,
-  createGatsbyIds,
+    standardizeKeys,
+    createEntityType,
+    createGatsbyIds,
 } from './normalize';
 
-export interface IGatsbyTransformerUploadcareOptions {
-  url: string;
-  publicKey: string;
-  localSave?: boolean;
-  idField?: string;
-  skipCreateNode?: boolean;
-  path?: string;
-  auth?: { username: string; password: string } | false;
-  payloadKey?: string;
-  name: string;
-  verboseOutput?: boolean;
+export interface IGatsbyTransformerUploadcareOptions extends PluginOptions {
+    url: string; // Uploadcare file metadata / info endpoint.
+    publicKey: string; // Uploadcare public key.
+    auth?: { username: string; password: string } | false; // Optional Auth payload for request.
+    name: string; // Name to append to nodes created by this plugin. 
+    searchTerm: string; // String to search for in fields, from which we infer that the field refers to an uploadcare asset with an info endpoint at the url option for this plugin.
 }
 
 const typePrefix = `transformerUploadcare__`;
 
-const isUploadCareUrl: (url: string) => boolean = url => {
-  return url.search('ucarecdn') === -1 ? false : true;
+const isMatchingSearchTerm: (searchTerm: string) => (url: string) => boolean = searchTerm => url => {
+    return url.search(searchTerm) === -1 ? false : true;
 };
 
 const objToArr: (obj: { [k: string]: any }) => any[] = obj => {
-  return Object.keys(obj).map(k => obj[k]);
+    return Object.keys(obj).map(k => obj[k]);
 };
 
-const myReducer: (acc: string[], curr: string) => string[] = (acc, curr) => {
-  if (typeof curr === 'string' && isUploadCareUrl(curr)) {
-    return [...acc, curr];
-  } else if (Array.isArray(curr)) {
-    const s = curr.reduce(myReducer, []); // Recur.
-    return [...acc, ...s];
-  } else if (typeof curr === 'object') {
-    const arr = objToArr(curr); // Convert to array.
-    const strs = arr.reduce(myReducer, []); // Recur.
-    return [...acc, ...strs];
-  }
-  return acc;
+const searchFields: (searchTerm: string) => (acc: string[], curr: {[k: string] : any}) => string[] = (searchTerm) => (acc, curr) => {
+    if (typeof curr === 'string' && isMatchingSearchTerm(searchTerm)(curr)) {
+        return [...acc, curr];
+    } else if (Array.isArray(curr)) {
+        const s = curr.reduce(searchFields(searchTerm), []); // Recur.
+        return [...acc, ...s];
+    } else if (typeof curr === 'object') {
+        const arr = objToArr(curr); // Convert to array.
+        const strs = arr.reduce(searchFields(searchTerm), []); // Recur.
+        return [...acc, ...strs];
+    }
+    return acc;
 };
 
 export const sourceNodes: GatsbyNode['sourceNodes'] = (
-  {
-    actions,
-    createNodeId,
-    reporter,
-    createContentDigest,
-    getNodes,
-  }: NodePluginArgs,
-  {
-    url,
-    publicKey,
-    idField = `id`,
-    localSave = false,
-    skipCreateNode = false,
-    path,
-    auth = false,
-    payloadKey,
-    name,
-    verboseOutput = false,
-  }: PluginOptions
+    {
+        actions,
+        createNodeId,
+        reporter,
+        createContentDigest,
+        getNodes,
+    }: NodePluginArgs,
+    {
+        url,
+        publicKey,
+        path,
+        name,
+        verboseOutput = false,
+        searchTerm
+    }: IGatsbyTransformerUploadcareOptions
 ) => {
-  const { createNode, createParentChildLink, createNodeField } = actions;
-  let entityType = `${typePrefix}${name}`;
+    const { createNode, createParentChildLink, createNodeField } = actions;
+    let entityType = `${typePrefix}${name}`;
 
-  const nodes = getNodes();
-  let allTheFetches: Promise<any>[] = [];
-  nodes.forEach((node: any) => {
-    if (
-      node.internal.type === 'MarkdownRemark' &&
-      node.frontmatter !== undefined
-    ) {
-      const fields: any[] = objToArr(node.frontmatter); // Convert to array.
-      const uploadCareUrls: string[] = fields.reduce(myReducer, []); // Reduce (has a recursion).
-      const uploadCareIds: string[] = uploadCareUrls.map(upUrl => {
-        return upUrl.split('/')[3];
-        //return `https://upload.uploadcare.com/info/?pub_key=${publicKey}\\&file_id=${uploadcareFileId}`
-      });
-      // For each uploadCareUrl there's a promise that resolves when its data is fetched.
-      uploadCareIds.map(id => {
-        allTheFetches.push(
-          fetch({
-            url: url as string,
-            uploadcareId: id,
-            publicKey: publicKey as string,
-            name: name as string,
-            localSave: localSave as boolean,
-            path: path as string,
-            verbose: verboseOutput as boolean,
-            reporter: reporter,
-            auth: false,
-            payloadKey: '',
-          }).then(e => {
-            let out = [e];
-            out = standardizeKeys(out);
-            out = createEntityType(entityType, out);
-            out = createGatsbyIds(createNodeId, out);
-            let n = out[0];
-            let { __type, ...entity } = n;
-            let uploadcareNode = {
-              ...entity,
-              id: n.id,
-              parent: null,
-              children: [],
-              mediaType: 'uploadcareMetaData',
-              internal: {
-                type: `transformerUploadcareMeta`,
-                contentDigest: createContentDigest(e),
-                description: `some kind of uploadcarefile`,
-                content: JSON.stringify(entity),
-              },
-            };
-            createNode(uploadcareNode);
-          })
-        );
-      });
-    }
-  });
-  debugger;
-  return Promise.all(allTheFetches);
+    const nodes = getNodes();
+
+    const allTheFetches = nodes.reduce((promises: Promise<any>[], node: Node) => {
+        let newPromises = [] as Promise<any>[]
+        if (
+            node.internal.type === 'MarkdownRemark' &&
+            node.frontmatter !== undefined
+        ) {
+            newPromises = objToArr(node.frontmatter).reduce(searchFields(searchTerm), []).map((uploadCareAssetUrl: string): string=> {
+                return uploadCareAssetUrl.split('/')[3]; // Return the uploadcare asset ID from the url.
+            }).reduce((fetches: Promise<any>[], id: string) => {
+                const newFetch = 
+                    fetch({
+                        url: url as string,
+                        uploadcareId: id,
+                        publicKey: publicKey as string,
+                        name: name as string,
+                        path: path as string,
+                        verbose: verboseOutput as boolean,
+                        reporter: reporter,
+                        auth: false,
+                    }).then(e => {
+
+                        const n = fp.compose(
+                            createEntityType(entityType),
+                            createGatsbyIds(createNodeId),
+                            standardizeKeys([e]),
+                        )()[0]
+
+                        let { __type, ...entity } = n;
+                        let uploadcareNode = {
+                            ...entity,
+                            id: n.id,
+                            parent: node.id,
+                            children: [],
+                            internal: {
+                                type: entityType,
+                                contentDigest: createContentDigest(e),
+                                description: `some kind of uploadcarefile`,
+                                content: JSON.stringify(entity),
+                            },
+                        };
+                        createNode(uploadcareNode);
+                        createParentChildLink({parent: node, child: uploadcareNode});
+                    })
+                return [...fetches, newFetch ]
+            }, [] as Promise<any>[]);
+        }
+        return [...promises, ...newPromises]
+    }, [] as Promise<any>[] )
+    // Gatsby's sourceNodes function should return a promise, so that Gatsby knows when all the nodes are present, and from which it can build the GraphQL schema.
+    return Promise.all(allTheFetches);
 };
