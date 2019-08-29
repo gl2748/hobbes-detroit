9554bbe83a86f249b0b7c51f82da105d283b3b48
Mike Schutte <tmikeschutte@gmail.com>
2019-08-29 11:31:20 -0400

    Use hero carousel in featured container

--- src/containers/FeaturedProjectRollContainer.tsx
+++ src/containers/FeaturedProjectRollContainer.tsx
@@ -1,12 +1,38 @@
 import { graphql, StaticQuery } from "gatsby";
 import React from "react";
-import { IProjectProps, ProjectRoll } from "../components/ProjectRoll";
+import { HeroCarousel } from "../components/HeroCarousel";
+import { HobLink } from "../components/HobLink";
+import { HobTypography } from "../components/HobTypography";
+import { IProjectProps } from "../components/ProjectRoll";
 import { IAllMarkdownRemark } from "./interfaces";
 
 export const FeaturedProjectRollContainer: React.FC = () => {
-  const render = (data: IAllMarkdownRemark<IProjectProps>): React.ReactNode => (
-    <ProjectRoll data={data} />
-  );
+  const render = (data: IAllMarkdownRemark<IProjectProps>): React.ReactNode => {
+    const { edges: posts } = data.allMarkdownRemark;
+
+    return (
+      <HeroCarousel>
+        {posts.map(({ node: post }: { node: IProjectProps }) => (
+          <div
+            key={post.id}
+            className={`${post.frontmatter.featured ? "is-featured" : ""}`}
+          >
+            <HobLink
+              color="dark-gray"
+              to={
+                post.frontmatter.protectedProject
+                  ? `/protected${post.fields.slug}`
+                  : post.fields.slug
+              }
+            >
+              {post.frontmatter.title}
+            </HobLink>
+          </div>
+        ))}
+      </HeroCarousel>
+    );
+  };
+
   return (
     <StaticQuery
       query={graphql`
