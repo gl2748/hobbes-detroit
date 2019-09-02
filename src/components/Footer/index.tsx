7689dbf19db77c42dc83258f5ea5fac7cde2834e
Mike Schutte <tmikeschutte@gmail.com>
2019-09-02 18:31:16 -0400

    Update Footer styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,18 +1,54 @@
-import { Link } from "gatsby";
+import styled from "@emotion/styled";
 import React from "react";
+import { GatsbyLink as Link } from "../GatsbyLink";
+import { HobLogo } from "../HobLogo";
+import { HobTypography } from "../HobTypography";
+
+const Container = styled.div`
+  position: relative;
+  z-index: 2;
+  display: flex;
+  padding: 0 2.5rem 1.8125rem;
+`;
+
+const Logo = styled.div`
+  flex: 2;
+  margin-right: 1rem;
+`;
+
+const Copyright = styled.div`
+  flex: 1;
+`;
+
+const Links = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  .hob-link {
+    margin-bottom: 0.375rem;
+  }
+`;
 
 export const Footer: React.FC = () => {
   return (
-    <ul>
-      <li>
-        <div>2019 (c) Hobbes</div>
-      </li>
-      <li>
-        <Link to="/legal">Legal Information</Link>
-      </li>
-      <li>
-        <Link to="/cookie">Cookie Policy</Link>
-      </li>
-    </ul>
+    <Container>
+      <Logo>
+        <HobLogo />
+      </Logo>
+
+      <Copyright>
+        <HobTypography variant="body1">2019 (c) Hobbes</HobTypography>
+      </Copyright>
+
+      <Links>
+        <Link to="/legal" color="primary">
+          Legal Information
+        </Link>
+        <Link to="/cookie" color="primary">
+          Cookie Policy
+        </Link>
+      </Links>
+    </Container>
   );
 };
