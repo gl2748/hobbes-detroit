b0f25f93dfc8944cd6a803a5169c7a58a116f753
Mike Schutte <tmikeschutte@gmail.com>
2019-09-02 16:54:31 -0400

    Studio sytyling

--- src/components/Studio/index.tsx
+++ src/components/Studio/index.tsx
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
 import React from "react";
+import breakpoints from "../../breakpoints";
+import { HobLink } from "../HobLink";
+import { HobTypography } from "../HobTypography";
+
 export interface IStudioProps {
   description: string;
   title: string;
@@ -13,6 +17,77 @@ export interface IStudioProps {
 const Container = styled.div`
   min-height: 100vh;
   background-color: var(--hob-color--secondary);
+  position: relative;
+  padding: 1.5rem 1.25rem;
+`;
+
+const Description = styled(HobTypography)`
+  flex: 1;
+  margin-right: 7.3125rem;
+
+  ${breakpoints.mobile} {
+    flex: unset;
+    margin-right: 0;
+    margin-bottom: 3rem;
+  }
+`;
+
+const Inner = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+
+  .hob-typography {
+    &--h1 {
+      width: 100%;
+      margin-bottom: 4.4375rem;
+    }
+    &--body1,
+    &--link {
+      font-size: 1.75rem;
+    }
+
+    &--caption {
+      font-size: 1.125rem;
+    }
+  }
+  .hob-link {
+    text-decoration: none;
+  }
+
+  ${breakpoints.mobile} {
+    & > * {
+      width: 100%;
+      margin-right: 0;
+    }
+  }
+`;
+
+const Field = styled.div`
+  flex: 1;
+  display: flex;
+  flex-wrap: wrap;
+
+  .hob-typography {
+    width: 100%;
+
+    &--caption {
+      margin-bottom: 2rem;
+      ${breakpoints.mobile} {
+        margin-bottom: 0;
+      }
+    }
+
+    &--body1 {
+      margin-bottom: 3rem;
+    }
+  }
+`;
+
+const Contact = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Studio: React.FC<IStudioProps> = ({
@@ -24,14 +99,40 @@ export const Studio: React.FC<IStudioProps> = ({
   social
 }: IStudioProps) => {
   return (
-    <Container id="studio">
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <p>{address}</p>
-      <p>{phone}</p>
-      <p>{email}</p>
-      <p>{social[0].title}</p>
-      <p>{social[0].url}</p>
+    <Container>
+      <Inner>
+        <HobTypography variant="h1">{title}</HobTypography>
+
+        <Description variant="body1">{description}</Description>
+
+        <Field>
+          <HobTypography variant="caption">Address</HobTypography>
+          <HobTypography variant="body1">{address}</HobTypography>
+        </Field>
+
+        <Contact>
+          <Field>
+            <HobTypography variant="caption">Tel</HobTypography>
+            <HobTypography variant="body1">{phone}</HobTypography>
+          </Field>
+
+          <Field>
+            <HobTypography variant="caption">Email</HobTypography>
+            <HobTypography variant="body1">{email}</HobTypography>
+          </Field>
+
+          <Field>
+            <HobTypography variant="caption">Follow us</HobTypography>
+            {social.map(({ title: label, url }) => (
+              <>
+                <HobLink color="primary" href={url}>
+                  {label}
+                </HobLink>
+              </>
+            ))}
+          </Field>
+        </Contact>
+      </Inner>
     </Container>
   );
 };
