b74e2700c1a1b94b31aee3e6e145bf4cd9a4e51c
Mike Schutte <tmikeschutte@gmail.com>
2019-09-03 09:34:33 -0400

    Add prev/next project links to project

--- src/templates/project-post.tsx
+++ src/templates/project-post.tsx
@@ -1,11 +1,24 @@
+import styled from "@emotion/styled";
 import { graphql } from "gatsby";
 import React from "react";
 import Helmet from "react-helmet";
+import breakpoints from "../breakpoints";
 import { HTMLContent } from "../components/Content";
+import { GatsbyLink as Link } from "../components/GatsbyLink";
+import { HobTypography } from "../components/HobTypography";
 import { Layout } from "../components/Layout";
 import { Project } from "../components/Project";
 import { WithAuth } from "../higherOrderComponents/WithAuth";
 
+interface SideLink {
+  frontmatter: {
+    protectedProject: boolean;
+    title: string;
+  };
+  fields: {
+    slug: string;
+  };
+}
 export interface IProjectPostProps {
   data: {
     markdownRemark: {
@@ -18,19 +31,132 @@ export interface IProjectPostProps {
         featured: boolean;
       };
     };
+    prev: SideLink;
+    next: SideLink;
   };
 }
+
+const PaginationContainer = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  height: 100vh;
+  width: 0.625rem;
+  transition: width var(--hob-transition-duration);
+  z-index: 4;
+
+  ${breakpoints.mobile} {
+    bottom: 0;
+    height: 0;
+  }
+
+  &:hover,
+  &:focus {
+    width: 2.5rem;
+  }
+
+  .side-pagination {
+    &__arrow {
+      position: absolute;
+      left: 0.5rem;
+      font-size: 1.3rem;
+    }
+
+    &__link {
+      position: absolute;
+      height: 50%;
+      right: 0;
+      width: 100%;
+
+      .hob-typography {
+        display: block;
+        transform: rotate(-90deg);
+        transform-origin: 0 0;
+        white-space: nowrap;
+        position: absolute;
+        left: 0.5rem;
+      }
+
+      &--top {
+        top: 0;
+        background-color: var(--hob-color--blue);
+
+        .hob-typography {
+          bottom: 0;
+        }
+
+        .side-pagination__arrow {
+          top: 0.875rem;
+        }
+      }
+
+      &--bottom {
+        bottom: 0;
+        background-color: var(--hob-color--pink);
+
+        .hob-typography {
+          bottom: 2.625rem;
+        }
+
+        .side-pagination__arrow {
+          bottom: 0.875rem;
+        }
+      }
+    }
+  }
+`;
+
+interface ISideLink {
+  to: string;
+  label: string;
+}
+const SidePagination = ({
+  prev,
+  next
+}: {
+  prev: ISideLink;
+  next: ISideLink;
+}) => {
+  return (
+    <PaginationContainer className="side-pagination">
+      <Link
+        to={next.to}
+        color="primary"
+        className="side-pagination__link side-pagination__link--top"
+        unsetTypography={true}
+      >
+        <span className="side-pagination__arrow">→</span>
+        <HobTypography variant="link">{next.label}</HobTypography>
+      </Link>
+
+      <Link
+        to={prev.to}
+        color="primary"
+        className="side-pagination__link side-pagination__link--bottom"
+        unsetTypography={true}
+      >
+        <span className="side-pagination__arrow">←</span>
+        <HobTypography variant="link">{prev.label}</HobTypography>
+      </Link>
+    </PaginationContainer>
+  );
+};
 
 const ProjectPost: React.FC<IProjectPostProps> = ({
   data
 }: IProjectPostProps) => {
-  const { markdownRemark: post } = data;
+  const { markdownRemark: post, prev, next } = data;
+  const toProps = (p: SideLink) => ({
+    label: p.frontmatter.title,
+    to: `${p.frontmatter.protectedProject ? "/protected" : ""}${p.fields.slug}`
+  });
 
   const EnhancedProjectComponent = post.frontmatter.protectedProject
     ? WithAuth(Project)
     : Project;
   return (
     <Layout>
+      <SidePagination prev={toProps(prev)} next={toProps(next)} />
       <EnhancedProjectComponent
         content={post.html}
         contentComponent={HTMLContent}
@@ -57,7 +183,7 @@ export default ProjectPost;
 
 // The $id param here comes from gatsby-node.js createPage method with a context property in the first argument.
 export const pageQuery = graphql`
-  query ProjectPostByID($id: String!) {
+  query ProjectPostByID($id: String!, $prevId: String!, $nextId: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
@@ -67,6 +193,24 @@ export const pageQuery = graphql`
         description
         tags
         protectedProject
+      }
+    }
+    prev: markdownRemark(id: { eq: $prevId }) {
+      frontmatter {
+        protectedProject
+        title
+      }
+      fields {
+        slug
+      }
+    }
+    next: markdownRemark(id: { eq: $nextId }) {
+      frontmatter {
+        protectedProject
+        title
+      }
+      fields {
+        slug
       }
     }
   }
