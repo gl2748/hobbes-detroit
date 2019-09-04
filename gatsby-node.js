c7566a691a484eac69e6c454817c1f17448dab3f
/ /\ / /\/ <imaitland@gmail.com>
2019-09-04 12:27:18 -0400

    misc cleanup, ts gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 "use strict";
 require("source-map-support").install();
 
-exports.createPages = require('./onCreateWebpackConfig').onCreateWebpackConfig;
 exports.createPages = require("./createPages").createPages;
 exports.onCreateNode = require("./onCreateNode").onCreateNode;
+exports.onCreateWebpackConfig = require("./onCreateWebpackConfig").onCreateWebpackConfig;
