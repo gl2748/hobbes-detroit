83949b22e47efe46d6c94711cf5f546be055ca25
Mike Schutte <tmikeschutte@gmail.com>
2019-09-14 15:14:46 -0400

    Fix large media padding

--- src/components/HobLargeMedia/index.tsx
+++ src/components/HobLargeMedia/index.tsx
@@ -14,7 +14,6 @@ export interface TLargeMediaStyles {
 const LargeMedia = styled.div<TLargeMediaStyles>`
   width: 100vw;
   position: relative;
-  padding: 1.25rem 6.625rem; // Default
   img {
     width: 100%;
   }
@@ -24,7 +23,7 @@ const LargeMedia = styled.div<TLargeMediaStyles>`
           padding: 1.25rem 0;
         `
       : css`
-          padding: 1.25rem 6.625rem;
+          padding: 1.25rem;
         `}
 `;
 
