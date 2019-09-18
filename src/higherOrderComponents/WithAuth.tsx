302914c49eb0352e630ae49fffe6879aa0948924
/ /\ / /\/ <imaitland@gmail.com>
2019-09-18 18:05:46 -0400

    use context in the WithAuth HOC

--- src/higherOrderComponents/WithAuth.tsx
+++ src/higherOrderComponents/WithAuth.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useIdentityContext } from "react-netlify-identity-widget";
+import { AppContext } from "../components/Layout";
 
 export const WithAuth = <P extends object>(
   Component: React.ComponentType<P>
 ): React.FC<P> => ({ ...props }) => {
   const identity = useIdentityContext();
   const isLoggedIn = identity && identity.isLoggedIn;
+  const { toggleDrawer } = useContext(AppContext);
 
   if (isLoggedIn) {
     return <Component {...(props as P)} />;
@@ -13,6 +15,7 @@ export const WithAuth = <P extends object>(
     return (
       <div>
         <h1>PLEASE SIGN IN!</h1>
+        {toggleDrawer && toggleDrawer()}
       </div>
     );
   }
