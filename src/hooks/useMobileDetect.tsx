e0f073af9e1f5ba2a3ca12d4fde7ae23cabd7272
/ /\ / /\/ <imaitland@gmail.com>
2019-10-09 09:18:05 -0400

    gatsby is server rendered

--- src/hooks/useMobileDetect.tsx
+++ src/hooks/useMobileDetect.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react";
-
 const getMobileDetect = (userAgent: string) => {
   const isAndroid = () => Boolean(userAgent.match(/Android/i));
   const isIos = () => Boolean(userAgent.match(/iPhone|iPad|iPod/i));
   const isOpera = () => Boolean(userAgent.match(/Opera Mini/i));
   const isWindows = () => Boolean(userAgent.match(/IEMobile/i));
+  const isServer = () => Boolean(userAgent.match(/server/i));
 
   const isMobile = () =>
     Boolean(isAndroid() || isIos() || isOpera() || isWindows());
@@ -13,11 +12,15 @@ const getMobileDetect = (userAgent: string) => {
     isAndroid,
     isDesktop,
     isIos,
-    isMobile
+    isMobile,
+    isServer
   };
 };
 const useMobileDetect = () => {
-  return getMobileDetect(navigator.userAgent);
+  const clientSide = !!navigator;
+  return clientSide
+    ? getMobileDetect(navigator.userAgent)
+    : getMobileDetect("server");
 };
 
 export default useMobileDetect;
