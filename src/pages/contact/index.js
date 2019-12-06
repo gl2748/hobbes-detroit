005ccfa61a1a021f31bbabe433b25218afb9d513
/ /\ / /\/ <imaitland@gmail.com>
2019-12-06 08:20:53 -0500

    add fetch polyfill

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,37 +1,38 @@
-import React from 'react'
-import { navigate } from 'gatsby-link'
-import {Layout} from '../../components/Layout'
+import React from "react";
+import { navigate } from "gatsby-link";
+import { fetch as fetchPolyfill } from "whatwg-fetch";
+import { Layout } from "../../components/Layout";
 
 function encode(data) {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 export default class Index extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { isValidated: false }
+    super(props);
+    this.state = { isValidated: false };
   }
 
   handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    e.preventDefault();
+    const form = e.target;
+    fetchPolyfill("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
     })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch(error => alert(error))
-  }
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
   render() {
     return (
@@ -52,41 +53,41 @@ export default class Index extends React.Component {
                 <input type="hidden" name="form-name" value="contact" />
                 <div hidden>
                   <label>
-                    Don’t fill this out:{' '}
+                    Don’t fill this out:{" "}
                     <input name="bot-field" onChange={this.handleChange} />
                   </label>
                 </div>
                 <div>
-                  <label htmlFor={'name'}>Your name</label>
+                  <label htmlFor={"name"}>Your name</label>
                   <div>
                     <input
-                      type={'text'}
-                      name={'name'}
+                      type={"text"}
+                      name={"name"}
                       onChange={this.handleChange}
-                      id={'name'}
+                      id={"name"}
                       required={true}
                     />
                   </div>
                 </div>
                 <div>
-                  <label htmlFor={'email'}>Email</label>
+                  <label htmlFor={"email"}>Email</label>
                   <div>
                     <input
-                      type={'email'}
-                      name={'email'}
+                      type={"email"}
+                      name={"email"}
                       onChange={this.handleChange}
-                      id={'email'}
+                      id={"email"}
                       required={true}
                     />
                   </div>
                 </div>
                 <div>
-                  <label htmlFor={'message'}>Message</label>
+                  <label htmlFor={"message"}>Message</label>
                   <div>
                     <textarea
-                      name={'message'}
+                      name={"message"}
                       onChange={this.handleChange}
-                      id={'message'}
+                      id={"message"}
                       required={true}
                     />
                   </div>
@@ -99,6 +100,6 @@ export default class Index extends React.Component {
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
